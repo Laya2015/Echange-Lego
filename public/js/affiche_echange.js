4948158762fade6842c2cc875bb0bd1aeafd4206
Anisse <96845859+Laya2015@users.noreply.github.com>
2024-11-08 11:48:56 -0500

    travail sur la page echanges pour coder le lien voir echange + css

--- public/js/affiche_echange.js
+++ public/js/affiche_echange.js
@@ -1,33 +1,25 @@
-// const btnVoirEchange = document.querySelectorAll("input");
-const notreTable = document.querySelector("table");
-// const tdquantite = document.querySelectorAll("#id_quantite");
-// const tdvaleur = document.querySelectorAll("#id_valeur");
 const trligne = document.querySelectorAll("#id_ligne");
 
-// console.log(tdquantite.innerText);
-// console.log(tdvaleur.innerText);
-// document.querySelectorAll('#id_ligne').forEach(element => {
-//     // Ton code ici pour chaque élément
-//     console.log(element);
-//   });
+
+
+function addTodoClient(){
 let somme =0;
 
 trligne.forEach(ligne => {
     const quantite = ligne.querySelector("#id_quantite").textContent;
-    console.log(quantite);
+    // console.log(quantite);
+
     const valeur = ligne.querySelector('#id_valeur').textContent;
-    console.log(valeur);
-    console.log(quantite*valeur);
-     const resultat = ligne.querySelector("#id_resultat");
-     let multiplication = (quantite*valeur)
-     resultat.textContent = multiplication.toFixed(2) + "$" ;
+    // console.log(valeur);
 
-     somme+=multiplication;
-     
+    // console.log(quantite*valeur);
+    const resultat = ligne.querySelector("#id_resultat");
+    let multiplication = (quantite*valeur)
+    resultat.textContent = multiplication.toFixed(2) + "$" ;
+    somme += multiplication;
+    });
 
-});
 const totalValeurEchange = document.querySelector("#totalValeurEchange");
 totalValeurEchange.textContent += somme.toFixed(2) + "$";
-
-
-// addTodoClient();
+}
+addTodoClient();
